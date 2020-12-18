9474f5295c09979cae76a1ae9d4841abd431d379
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-18 13:32:46 +0000

    End code.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 import StoryList from './components/StoryList';
 
-
+// Container for articleids and stories
 function App() {
 
   const [articleIds, setArticleIds] = useState([]);
@@ -10,23 +10,21 @@ function App() {
 
   const fetchStoryData = (storyIds, numStories) => {
     // 'extract' the first 4 id's
-    const topFour = storyIds.slice(0, 4);
+    const topStories = storyIds.slice(0, numStories);
     //console.log(topFour);
 
     // map every url to the promise of the fetch
-    let promises = topFour.map(url => fetch(`https://hacker-news.firebaseio.com/v0/item/${url}.json`));
+    let promises = topStories.map(storyId => fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`));
 
-    // create a map of stories (array), to use in the forEach loop below.
-    const clonedStories = stories.map((element) => {
-      return element
-    });
+    // // create a map of stories (array), to use in the forEach loop below.
+    // const clonedStories = stories.map((element) => {
+    //   return element
+    // });
 
-    // Promise.all to waits until all promises are resolved
-    Promise.all(promises)
-      .then(responses =>  responses)
+    // Promise.all to wait until *ALL* promises are resolved
+    Promise.all(promises) // accepts an array of promises and array of responses
       .then(responses => Promise.all(responses.map(r => r.json())))
-      .then(stories => setStories(stories))
-
+      .then(stories => setStories(stories));
     
     // save that story data to the state
   }
@@ -35,14 +33,15 @@ function App() {
   // effect of [].
   useEffect(()=>{
     fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
-    .then((res)=>res.json())
+    .then((res)=>res.json()) // res is a single response
     .then((data)=>{
       setArticleIds(data);
     });
   },[]);
 
+  // watch a part of the state variable (a single state variable - articleIds)
   useEffect(()=>{
-    fetchStoryData(articleIds, 10);
+    fetchStoryData(articleIds, 10); // fetch the number of the articles specified (the top x number of articles)
   },[articleIds])
 
   return (
